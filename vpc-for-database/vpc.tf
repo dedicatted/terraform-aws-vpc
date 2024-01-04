876b4ce13af6d64b8a06336ae518e558bccbde5a
DanyloSafankov <youremail@example.com>
2024-01-04 10:40:18 +0200

    Added data block to get azs

--- vpc-for-database/vpc.tf
+++ vpc-for-database/vpc.tf
@@ -1,8 +1,12 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 module "vpc" {
   source                             = "github.com/terraform-aws-modules/terraform-aws-vpc"
   name                               = var.vpc_name
   cidr                               = var.cidr_block
-  azs                                = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  azs                                = data.aws_availability_zones.available.names
   database_subnets                   = [cidrsubnet(var.cidr_block, 8, 6), cidrsubnet(var.cidr_block, 8, 7), cidrsubnet(var.cidr_block, 8, 8)]
   create_database_subnet_group       = true
   create_database_subnet_route_table = true
--- vpc-for-eks/vpc.tf
+++ vpc-for-eks/vpc.tf
@@ -1,8 +1,12 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 module "vpc" {
   source             = "github.com/terraform-aws-modules/terraform-aws-vpc"
   name               = var.vpc_name
   cidr               = var.cidr_block
-  azs                = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  azs                = data.aws_availability_zones.available.names
   public_subnets     = [cidrsubnet(var.cidr_block, 8, 3), cidrsubnet(var.cidr_block, 8, 4), cidrsubnet(var.cidr_block, 8, 5)]
   private_subnets    = [cidrsubnet(var.cidr_block, 8, 0), cidrsubnet(var.cidr_block, 8, 1), cidrsubnet(var.cidr_block, 8, 2)]
   enable_nat_gateway = true
